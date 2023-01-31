6f0c8bdd2681a0a9ed81d9c65d10497bf88c2c3b
PerfectPillows <rishi.chakrabarti@gmail.com>
2023-01-31 14:42:45 +0530

    added chart js

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -1,12 +1,24 @@
 import "./Navbar.css";
+import React from "react";
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 
 const Statistics = () => {
+  const labels = ["January", "February", "March", "April", "May", "June"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
   return (
     <div>
-      <ul>
-        <li>Phone Number: 0000000</li>
-        <li>Email id: bpcheck@oggle.in</li>
-      </ul>
+      <Bar data={data} />
     </div>
   );
 };
